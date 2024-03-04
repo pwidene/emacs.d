82f9f539a05645015e45f43b363c7b49bdb5d07c
Patrick Widener <widenerpm@ornl.gov>
2024-03-04 09:48:49 -0700

    custom.el

--- custom.el
+++ custom.el
@@ -18,7 +18,7 @@
  '(org-level-4 ((t (:inherit variable-pitch :height 1.05))))
  '(org-link ((t (:foreground "royal blue" :underline t))))
  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch :underline t))) t)
+ '(org-property-value ((t (:inherit fixed-pitch :underline t))))
  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
@@ -43,6 +43,8 @@
  '(package-selected-packages
    '(websocket json-navigator json-process-client json-reformat json-rpc json-snatcher jsonl jsonrpc rjsx-mode org-ql org-gcal org-super-agenda org-superstar org-sync org-sync-snippets org-tag-beautify gist transient org-roam-server zonokai-theme yasnippet-snippets yaml-mode web-mode use-package-ensure-system-package undo-tree underwater-theme ujelly-theme tronesque-theme treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil toxi-theme telephone-line tangotango-theme tango-plus-theme sublime-themes subatomic-theme spaceline spacegray-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smex smartparens smart-mode-line shell-pop seti-theme rustic rust-playground rainbow-mode racer project-explorer plantuml-mode persp-projectile paradox org-roam org-journal org-bullets oldlace-theme obsidian-theme nlinum night-owl-theme niflheim-theme neotree nav naquadah-theme nameframe-projectile nameframe-perspective mustang-theme multiple-cursors monokai-theme monochrome-theme molokai-theme moe-theme minimap magit-svn lsp-rust light-soap-theme json-mode js2-mode ivy-posframe ir-black-theme inkpot-theme idomenu ido-vertical-mode ido-ubiquitous icicles htmlize highlight hexrgb heroku-theme hc-zenburn-theme haskell-mode gruvbox-theme gnuplot-mode gnuplot git-messenger git-gutter gcmh gandalf-theme flymake-rust flycheck-rust flx-ido flatui-theme flatland-theme firebelly-theme espresso-theme display-theme dired+ dimmer diminish-buffer diminish deft darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme counsel color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-modern color-theme-buffer-local color-theme-approximate cmake-mode clues-theme centaur-tabs cargo busybee-theme bubbleberry-theme bookmark+ bind-map bind-chord autofit-frame auto-complete auctex-latexmk apples-mode anzu anti-zenburn-theme ample-zen-theme ample-theme all-the-icons afternoon-theme))
  '(plantuml-default-exec-mode 'executable)
- '(plantuml-executable-path "/opt/local/bin/plantuml"))
+ '(plantuml-executable-path "/opt/local/bin/plantuml")
+ '(safe-local-variable-values '((TeX-master . main.tex)))
+ '(warning-suppress-types '((comp) (comp))))
 
 		    
