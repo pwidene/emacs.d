fa6cc70121aa5e6f72708cd9e42fd378fde478f0
Patrick Widener <widenerpm@ornl.gov>
2024-12-06 11:45:51 -0700

    cleanups

--- custom.el
+++ custom.el
@@ -1,44 +1,4 @@
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background unspecified))))
- '(fixed-pitch ((t (:family "Hack"))))
- '(fringe ((t :background "#FFFFFF")))
- '(header-line ((t :box (:line-width 4 :color "#F0F0F0" :style unspecified))))
- '(header-line-highlight ((t :box (:color "#333333"))))
- '(keycast-key ((t)))
- '(line-number ((t :background "#FFFFFF")))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit variable-pitch :height 2.0 :underline unspecified))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-level-1 ((t (:inherit variable-pitch :height 1.5))))
- '(org-level-2 ((t (:inherit variable-pitch :height 1.25))))
- '(org-level-3 ((t (:inherit variable-pitch :height 1.1))))
- '(org-level-4 ((t (:inherit variable-pitch :height 1.05))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch :underline t))))
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "grey85" :style unspecified))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey75" :style unspecified))))
- '(tab-line-tab ((t)))
- '(tab-line-tab-active ((t)))
- '(tab-line-tab-current ((t)))
- '(tab-line-tab-inactive ((t)))
- '(variable-pitch ((t (:family "Trebuchet MS"))))
- '(vertical-border ((t :background "#FFFFFF" :foreground "#FFFFFF")))
- '(window-divider ((t (:background "#FFFFFF" :foreground "#FFFFFF"))))
- '(window-divider-first-pixel ((t (:background "#FFFFFF" :foreground "#FFFFFF"))))
- '(window-divider-last-pixel ((t (:background "#FFFFFF" :foreground "#FFFFFF")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -115,20 +75,5 @@
      ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
      ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
      ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
-     ("Other" "" TeX-run-command t t :help "Run an arbitrary command")))
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes
-   '("9bed3fb18c8d18acca50f3609efcaa8d1c72027802cec8d291d4577e3e48b825" "a1c18db2838b593fba371cb2623abd8f7644a7811ac53c6530eebdf8b9a25a8d" default))
- '(ispell-program-name "/opt/local/bin/aspell")
- '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/")))
- '(package-enable-at-startup nil)
- '(package-selected-packages
-   '(websocket json-navigator json-process-client json-reformat json-rpc json-snatcher jsonl jsonrpc rjsx-mode org-ql org-gcal org-super-agenda org-superstar org-sync org-sync-snippets org-tag-beautify gist transient org-roam-server zonokai-theme yasnippet-snippets yaml-mode web-mode use-package-ensure-system-package undo-tree underwater-theme ujelly-theme tronesque-theme treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil toxi-theme telephone-line tangotango-theme tango-plus-theme sublime-themes subatomic-theme spaceline spacegray-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smex smartparens smart-mode-line shell-pop seti-theme rustic rust-playground rainbow-mode racer project-explorer plantuml-mode persp-projectile paradox org-roam org-journal org-bullets oldlace-theme obsidian-theme nlinum night-owl-theme niflheim-theme neotree nav naquadah-theme nameframe-projectile nameframe-perspective mustang-theme multiple-cursors monokai-theme monochrome-theme molokai-theme moe-theme minimap magit-svn lsp-rust light-soap-theme json-mode js2-mode ivy-posframe ir-black-theme inkpot-theme idomenu ido-vertical-mode ido-ubiquitous icicles htmlize highlight hexrgb heroku-theme hc-zenburn-theme haskell-mode gruvbox-theme gnuplot-mode gnuplot git-messenger git-gutter gcmh gandalf-theme flymake-rust flycheck-rust flx-ido flatui-theme flatland-theme firebelly-theme espresso-theme display-theme dired+ dimmer diminish-buffer diminish deft darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme counsel color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-modern color-theme-buffer-local color-theme-approximate cmake-mode clues-theme centaur-tabs cargo busybee-theme bubbleberry-theme bookmark+ bind-map bind-chord autofit-frame auto-complete auctex-latexmk apples-mode anzu anti-zenburn-theme ample-zen-theme ample-theme all-the-icons afternoon-theme))
- '(plantuml-default-exec-mode 'executable)
- '(plantuml-executable-path "/opt/local/bin/plantuml")
- '(safe-local-variable-values '((TeX-master . main.tex)))
- '(warning-suppress-types '((comp) (comp))))
-
+     ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
 		    
